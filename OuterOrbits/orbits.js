211a246c91c4264ee46629281cd7cd5a2b56536c
DESKTOP-GTA06Q3\epdub <epdubi@gmail.com>
2017-12-12 10:12:24 -0600

    12/12/17

---
+++ OuterOrbits/orbits.js
@@ -0,0 +1,149 @@
+"use strict";
+
+var dateObject = new Date();
+
+function displayCalendar(whichMonth) {
+    var date;
+    var dateToday = new Date();
+    var dayOfWeek;
+    var daysInMonth;
+    var dateCells;
+    var captionValue;
+    var month;
+    var year;
+    var monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    if (whichMonth === -1) {
+        dateObject.setMonth(dateObject.getMonth() - 1);
+    } else if (whichMonth === 1) {
+        dateObject.setMonth(dateObject.getMonth() + 1);
+    } //End else/if
+    month = dateObject.getMonth();
+    year = dateObject.getFullYear();
+    dateObject.setDate(1);
+    dayOfWeek = dateObject.getDay();
+    captionValue = monthArray[month] + " " + year;
+    document.querySelector("#cal table caption").innerHTML = captionValue;
+    if (month === 0 || month === 2 || month === 4 || month === 6 || month === 7 || month === 9 || month === 11) {
+        daysInMonth = 31;
+    } else if (month === 1) {
+        if (year % 4 === 0) {
+            if (year % 100 === 0) {
+                //year ending in 00 not a leap year unless devisible by 400
+                if (year % 400 === 0) {
+                    daysInMonth = 29;
+                } else {
+                    daysInMonth = 28;
+                }
+            } else {
+                daysInMonth = 29;
+            }
+        } else {
+            daysInMonth = 28;
+        }
+    } else {
+        daysInMonth = 30;
+    }
+    dateCells = document.getElementsByTagName("td");
+    for (var i = 0; i < dateCells.length; i++) {
+        dateCells[i].innerHTML = "";
+        dateCells[i].className = "";
+    }
+    for (var i = dayOfWeek; i < daysInMonth + dayOfWeek; i++) {
+        dateCells[i].innerHTML = dateObject.getDate();
+        dateCells[i].className = "date";
+        if (dateToday < dateObject) {
+            dateCells[i].className = "futuredate";
+        }
+        date = dateObject.getDate() + 1;
+        dateObject.setDate(date);
+    }
+    dateObject.setMonth(dateObject.getMonth() - 1);
+    //reset month to month shown
+    document.getElementById("cal").style.display = "block";
+    //display calendar if it's not already visable
+
+} //End displayCalendar
+
+function selectDate(event) {
+    if (event === undefined) { //get caller element IE8
+        event = window.event;
+
+    }
+    var callerElement = event.target || event.srcElement;
+    if (callerElement.innerHTML === "") {
+        //cell contains no date, so don't close the calendar
+        document.getElementById("cal").style.display = "block";
+        return false;
+    }
+    dateObject.setDate(callerElement.innerHTML);
+    var fullDateToday = new Date();
+    var dateToday = Date.UTC(fullDateToday.getFullYear(), fullDateToday.getMonth(), fullDateToday.getDate());
+    var selectedDate = Date.UTC(dateObject.getFullYear(), dateObject.getMonth(), dateObject.getDate());
+    if (selectedDate <= dateToday) {
+        document.getElementById("cal").style.display = "block";
+        return false;
+    }
+    document.getElementById("tripDate").value = dateObject.toLocaleDateString();
+    hideCalendar();
+    updateTotalCost();
+}
+
+function hideCalendar() {
+    document.getElementById("cal").style.display = "none";
+}
+
+function prevMo() {
+    displayCalendar(-1);
+}
+
+function nextMo() {
+    displayCalendar(1);
+}
+
+function createEventListeners() {
+    var dateField = document.getElementById("tripDate");
+    if (dateField.addEventListener) {
+        dateField.addEventListener("click", displayCalendar, false);
+    } else if (dateField.attachEvent) {
+        dateField.attachEvent("onclick", displayCalendar);
+    }
+    var dateCells = document.getElementsByTagName("td");
+    if (dateCells[0].addEventListener) {
+        for (var i = 0; i < dateCells.length; i++) {
+            dateCells[i].addEventListener("click", selectDate, false);
+        }
+    } else if (dateCells[0].attachEvent) {
+        for (var i = 0; i < dateCells.length; i++) {
+            dateCells[i].attachEvent("onclick", selectDate);
+        }
+    }
+    var closeButton = document.getElementById("close");
+    if (closeButton.addEventListener) {
+        closeButton.addEventListener("click", hideCalendar, false);
+    } else if (closeButton.attachEvent) {
+        closeButton.attachEvent("onclick", hideCalendar);
+    }
+    var prevLink = document.getElementById("prev");
+    var nextLink = document.getElementById("next");
+    if (prevLink.addEventListener) {
+        prevLink.addEventListener("click", prevMo, false);
+        nextLink.addEventListener("click", nextMo, false);
+    } else if (prevLink.attachEvent) {
+        prevLink.attachEvent("onclick", prevMo);
+        nextLink.attachEvent("onclick", nextMo);
+    }
+}
+
+function updateTotalCost() {
+    var totalCost = 250000;
+    var monthlyCost = totalCost / 60;
+    var shortMonthlyCost = monthlyCost.toFixed(0);
+    document.getElementById("singleLabel").innerHTML = "Single payment of $" + totalCost.toLocaleString();
+    document.getElementById("multipleLabel").innerHTML = "60 monthly payments of $" + shortMonthlyCost.toLocaleString();
+}
+
+if (window.addEventListener) {
+    window.addEventListener("load", createEventListeners, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", createEventListeners);
+}
